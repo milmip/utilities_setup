0aefa584dfd52332ce7f8cd6f75f066c059de40b
Emilien Progin <emilien.progin@etik.com>
2026-04-10 18:53:42 +0200

    Ajout des scripts python

---
+++ latex/install_depedency.sh
@@ -0,0 +1,6 @@
+#bin/bash
+
+apt update
+apt install texlive-full rofi
+
+
---
+++ latex/scripts/rofi-lectures.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+from courses import Courses
+from rofi import rofi
+from utils import generate_short_title, MAX_LEN
+
+lectures = Courses().current.lectures
+
+sorted_lectures = lectures#sorted(lectures, key=lambda l: -l.number)
+
+options = [
+    "{number: >2}. <b>{title: <{fill}}</b> <span size='smaller'>{date}  ({week})</span>".format(
+        fill=MAX_LEN,
+        number=lecture.number,
+        title=generate_short_title(lecture.title),
+        date=lecture.date.strftime('%a %d %b'),
+        week=lecture.week
+    )
+    for lecture in sorted_lectures
+]
+
+key, index, selected = rofi('Select lecture', options, [
+    '-lines', 5,
+    '-markup-rows',
+    '-kb-row-down', 'Down',
+    '-kb-custom-1', 'Ctrl+n'
+])
+
+if key == 0:
+    if index == 0:
+        lectures.update_lectures_in_master([index+1])
+    else :
+        lectures.update_lectures_in_master([index, index+1])
+    sorted_lectures[index].edit()
+elif key == 1:
+    new_lecture = lectures.new_lecture()
+    new_lecture.edit()
